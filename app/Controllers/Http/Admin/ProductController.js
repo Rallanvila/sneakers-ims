946711e891e8c34a0e59cf0c7f704ce241484865
Allan Vila <allanvila@Allans-MBP.tampabay.rr.com>
2022-03-03 00:08:12 -0500

    setup product tables and product ui

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -1,21 +1,54 @@
 "use strict";
+const Database = use("Database");
+const sanitize = require("sqlstring");
 
 class ProductController {
-  index({ view }) {
-    return view.render("admin/products/all");
+  async index({ view, request, response }) {
+    try {
+      let allProducts = await Database.raw(`SELECT * FROM products`);
+      allProducts = allProducts[0];
+      return view.render("admin/products/all", { allProducts });
+    } catch (error) {
+      return response.redirect("back");
+      //   `<h1 style="color:red">There was an error:</h1>
+      //       <h3>${error.sqlMessage}</h3>`;
+    }
   }
-  store() {}
-  create({ view }) {
+  async store({ request, response }) {
+    try {
+      const post = request.post();
+      await Database.raw(
+        `
+                INSERT INTO products (title, sku, material, qty, description, brand_id, size, user_id)
+                Values(
+                    ${sanitize.escape(post.title)},
+                    ${sanitize.escape(post.sku)},
+                    ${sanitize.escape(post.material)},
+                    ${sanitize.escape(post.qty)},
+                    ${sanitize.escape(post.description)},
+                    ${parseInt("1")},
+                    ${sanitize.escape(post.size)},
+                    ${parseInt("1")})
+            `
+      );
+      return response.redirect("/admin/products");
+    } catch (error) {
+      return response.redirect("back");
+      //   `<h1 style="color:red">There was an error:</h1>
+      //       <h3>${error.sqlMessage}</h3>`;
+    }
+  }
+  create({ view, request, response }) {
     return view.render("admin/products/create");
   }
-  show({ view }) {
+  show({ view, request, response }) {
     return view.render("admin/products/show");
   }
-  edit({ view }) {
+  edit({ view, request, response }) {
     return view.render("admin/products/edit");
   }
-  update() {}
-  delete() {}
+  update({ request, response }) {}
+  delete({ request, response }) {}
 }
 
 module.exports = ProductController;
