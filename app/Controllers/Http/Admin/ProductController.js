8848f510d56e952439f1143f8d9eed7b71a7ad49
Allan Vila <allanvila@Allans-MBP.tampabay.rr.com>
2022-03-04 01:19:40 -0500

    finished product integration

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -93,13 +93,82 @@ class ProductController {
       //   `<h1 style="color:red">There was an error:</h1>
       //       <h3>${error.sqlMessage}</h3>`;
     }
-    return view.render("admin/products/show");
   }
-  edit({ view, request, response }) {
-    return view.render("admin/products/edit");
+  async edit({ view, request, response, params }) {
+    try {
+      let product = await Database.raw(`
+            SELECT 
+                products.id,
+                products.title,
+                products.sku,
+                products.img_url,
+                products.description,
+                products.material,
+                products.qty,
+                products.size,
+                products.user_id,
+                products.created_at,
+                brands.title as brand,
+                CONCAT(users.f_name, ' ', users.l_name) as user
+            FROM products
+            INNER JOIN brands
+            ON products.brand_id = brands.id
+            INNER JOIN users
+            ON products.user_id = users.id
+            WHERE products.id = ${params.id}
+            ORDER BY created_at ASC
+            LIMIT 1
+          `);
+      product = product[0][0];
+      return view.render("admin/products/edit", { product });
+    } catch (error) {
+      return response.redirect("back");
+      //   `<h1 style="color:red">There was an error:</h1>
+      //       <h3>${error.sqlMessage}</h3>`;
+    }
   }
-  update({ request, response }) {}
-  delete({ request, response }) {}
+  async update({ request, response, params }) {
+    try {
+      const id = params.id;
+      const post = request.post();
+      await Database.raw(
+        `
+            UPDATE products
+            SET 
+                title = ${sanitize.escape(post.title)},
+                sku = ${sanitize.escape(post.sku)},
+                img_url = ${sanitize.escape(post.img_url)},
+                material = ${sanitize.escape(post.material)},
+                qty = ${sanitize.escape(post.qty)},
+                description = ${sanitize.escape(post.description)},
+                brand_id = ${parseInt("1")},
+                size = ${sanitize.escape(post.size)},
+                user_id = ${parseInt("1")}
+            WHERE id = ${id}
+            `
+      );
+      return response.redirect(`/admin/products/${id}`);
+    } catch (error) {
+      console.log(error);
+      return response.redirect("back");
+    }
+  }
+  async delete({ request, response, params }) {
+    try {
+      const id = params.id;
+      const post = request.post();
+      await Database.raw(
+        `
+            DELETE FROM products
+            WHERE id = ${id}
+            `
+      );
+      return response.redirect(`/admin/products`);
+    } catch (error) {
+      console.log(error);
+      return response.redirect("back");
+    }
+  }
 }
 
 module.exports = ProductController;
--- start/routes.js
+++ start/routes.js
@@ -18,15 +18,10 @@ const Route = use("Route");
 
 // Route.on('/').render('welcome')
 Route.get("/", "PageController.index");
-
 Route.get("/admin/products", "Admin/ProductController.index");
 Route.post("/admin/products", "Admin/ProductController.store");
-
 Route.get("/admin/products/create", "Admin/ProductController.create");
 Route.get("/admin/products/:id", "Admin/ProductController.show");
-Route.get("admin/products/:id", "Admin/ProductController.show");
-Route.get("admin/products/:id/create", "Admin/ProductController.show");
-Route.get("admin/products/:id/edit", "Admin/ProductController.show");
 Route.get("/admin/products/:id/edit", "Admin/ProductController.edit");
-Route.put("/admin/products/:id/edit", "Admin/ProductController.update");
+Route.put("/admin/products/:id", "Admin/ProductController.update");
 Route.get("/admin/products/:id/delete", "Admin/ProductController.delete");
